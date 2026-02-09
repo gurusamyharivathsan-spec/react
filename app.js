cc6b4bcd1b01c47f175ab31acb4f392384a7c8c2
DELL <gurusamy.harivathsan@gmail.com>
2026-02-09 17:24:35 +0530

    Run using parcel done

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-/* div parent
-        div child
-            h1
-            h2
-        div child2
-            h1
-            h2
-*/
-
-const parent = React.createElement("div",{},
-    [React.createElement("div", {}, 
-        [React.createElement("h1", {}, "Hello from h1"), 
-            React.createElement("h2",{}, "Hello from h2")]),
-        React.createElement("div2", {}, 
-        [React.createElement("h1", {}, "Hello from h1"), 
-            React.createElement("h2",{}, "Hello from h2")])])
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div",{id : "parent"},
+    [React.createElement("div", {id : "child1"}, [
+        React.createElement("h1",{}, "Hello from H1 Tag"),
+        React.createElement("h2", {}, "Hello from H2 Tag")
+    ]),
+    React.createElement("div", {id : "child2"}, [
+        React.createElement("h1", {}, "Hello from h1 Tag"),
+        React.createElement("h2", {}, "Hello from h2 Tag")
+    ])]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
